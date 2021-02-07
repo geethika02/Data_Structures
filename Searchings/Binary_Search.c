66d6b1834018b726ce9664fb622cf8869e747957
Geethika Priya <75033631+geethika02@users.noreply.github.com>
2021-02-07 15:02:11 +0530

    Update Binary_Search.c

--- Searchings/Binary_Search.c
+++ Searchings/Binary_Search.c
@@ -1,65 +1,34 @@
 #include <stdio.h> 
-
 int binarysearch(int a[], int low, int high, int key)
- 
-{ 
-    
-        int flag=0;
-   
-        while(high>=low) 
+ { 
+     int flag=0;
+    while(high>=low) 
          { 
-
-                 int mid=(low+high)/2; 
-
-                 if(a[mid]==key) 
+	     int mid=(low+high)/2; 
+	     if(a[mid]==key) 
                  {
                     flag=1;
-		
-                    break;
-	
-	}
-	
-	else if(a[mid]>key)
-	
-	{
-		
-                     high=mid-1;
-	
-	}
-		
-                  else
-		
-                      low=mid+1;
-    
-        }
- 
-     if(flag==1)
-    
-              printf("Element is found");
-
-     else
-   
-             printf("Element is not found");
- 
-   } 
- 
-
+	            break;
+	         }
+	     else if(a[mid]>key)
+	         {
+	            high=mid-1;
+                 }
+	     else
+		    low=mid+1;
+         }
+ if(flag==1)
+    printf("Element is found");
+else
+   printf("Element is not found");
+ } 
 int main()
-
 {
-     
       int n,key,i;
-  
       scanf("%d",&n);
- 
       int a[n];
-    
       for(i=0;i<n;i++)
-   
       scanf("%d",&a[i]);
-   
       scanf("%d",&key);
- 
       binarysearch(a,0,n-1,key);
-
 }
