d9f7d2dcdff76838a0669ec09494fe07bd8b214b
Geethika Priya <75033631+geethika02@users.noreply.github.com>
2021-05-03 18:14:04 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,3 +1,90 @@
 /*
 Stack is one of the Linear DataStructure in which the operations are performed in the order of LIFO(Last In First Out) or FILO(First In Last Out).
 */
+#include <stdio.h>   
+int stack[100],i,j,choice=0,n,top=-1;  
+void push ()  
+{  
+    int val;      
+    if (top == n )   
+    printf("\n Overflow");   
+    else   
+    {  
+        printf("Enter the value?");  
+        scanf("%d",&val);         
+        top = top +1;   
+        stack[top] = val;   
+    }   
+}    
+void pop ()   
+{   
+    if(top == -1)   
+    printf("Underflow");  
+    else  
+    top = top -1;   
+}   
+void show()
+  {  
+    for (i=top;i>=0;i--)  
+    {  
+        printf("%d\n",stack[i]);  
+    }  
+    if(top == -1)   
+    {  
+        printf("Stack is empty");  
+    }  
+}  
+int peek()  
+{  
+    if (top == -1)   
+    {  
+        printf("Underflow");  
+        return 0;   
+    }  
+    else  
+    {  
+        printf("%d\n",stack [top]);  
+    }  
+}  
+int main()  
+{  
+    scanf("%d",&n);  
+    while(choice != 4)  
+    {   
+        printf("\n1.Push\n2.Pop\n3.Show\n4.Peek\n5.Exit");  
+        printf("\n Enter your choice \n");        
+        scanf("%d",&choice);  
+        switch(choice)  
+        {  
+            case 1:  
+            {   
+                push();  
+                break;  
+            }  
+            case 2:  
+            {  
+                pop();  
+                break;  
+            }  
+            case 3:  
+            {  
+                show();  
+                break;  
+            }  
+            case 4:
+            {
+              peek();
+              break;
+            }
+            case 5:   
+            {  
+                printf("Exit");  
+                break;   
+            }  
+            default:  
+            {  
+                printf("Enter 1 or 2 or 3 or 4 or 5 ");  
+            }   
+        };  
+    }  
+}   
