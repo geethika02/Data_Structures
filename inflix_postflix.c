1e8557c143b32b5df0e789092bbd2e3574079179
Geethika Priya <75033631+geethika02@users.noreply.github.com>
2021-05-14 11:17:56 +0530

    Update inflix_postflix.c

--- inflix_postflix.c
+++ inflix_postflix.c
@@ -1,177 +1,95 @@
 #include<stdio.h>
-
 #include<stdlib.h>
-
 #define MAX 1005
-
- 
 char stack[MAX];
-
  int top=-1;
-
- 
 int priority(char a)
 {
-
-	if(a=='(')
-	
-      return -1;
+	if(a=='(')	
+      	 return -1;
 
 	if(a=='+')
-	
-      return 0;
+	 return 0;
 
 	if(a=='-')
-
-	    return 1;
+	 return 1;
 	
-  if(a=='*')
-
-	    return 2;
+  	if(a=='*')
+	 return 2;
 
 	if(a=='/')
-
-	    return 3;
+	 return 3;
 
 	if(a=='^')
-
-	     return 4;
-}
-
- 
+	 return 4;
+} 
 void push(char a)
 {
-
 	if(top==MAX-1)
-
-	return;
-
+	  return;
 	else
-
-	{
-
-		top=top+1;
-
+	{	top=top+1;
 		stack[top]=a;
-
 	}
 }
-
- 
 char pop()
 {
-
 	if(top==-1)
-
-	return 1;
-
+	 return 1;
 	else
-
 	{
-
 		char val=stack[top];
-
 		top=top-1;
-
 		return val;
-
 	}
 }
-
- 
 int isempty()
 {
-
 	if(top==-1)
-
-	return 1;
-
+	 return 1;
 	else
-
-	return 0;
+	 return 0;
 }
-
- 
 char str[MAX];
-
- 
 int main()
 {
-
 	scanf("%s",str);
-
 	int i;
-
 	for(i=0;str[i]!='\0';i++)
-
 	{
 
 		if(str[i]>='a'&&str[i]<='z')
-
-		printf("%c",str[i]);
-
+		  printf("%c",str[i]);
 		else if(str[i]=='(')
-
-		push(str[i]);
-
+		  push(str[i]);
 		else if(str[i]==')')
-
 		{
-
 			char c=pop();
-
-			//printf("---%c",c);
-
 			while(c!='(')
-
 			{
 
 				printf("%c",c);
-
 				c=pop();
-
 			}
-
 		}
-
 		else
- 
-		{
-
-		if(priority(str[i])>priority(stack[top]))
-
-		push(str[i]);
-
-		else
-
-		{
-
-		 while(priority(str[i])<priority(stack[top])&&stack[top]!='(')
-
-		 {
-
-		 	char c=pop();
-
-		 	printf("%c",c);
-
-		 }
-
-		 push(str[i]);
-
+ 		{
+			if(priority(str[i])>priority(stack[top]))
+			  push(str[i]);
+			else
+			{
+				 while(priority(str[i])<priority(stack[top])&&stack[top]!='(')
+				 {
+					 char c=pop();
+					printf("%c",c);
+				 }
+				push(str[i]);
+			}
 		}
-
-		}
-
 	}
-
 	while(!isempty())
-
 	{
-
 		printf("%c",pop());
-
 	}
-	
 	return 0;
-
 }
